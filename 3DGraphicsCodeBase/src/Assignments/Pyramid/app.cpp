260308773c1da23391134589029b287dc38263eb
kolodziejpawel98 <kolodziejpawel98@gmail.com>
2021-11-12 15:39:40 +0100

    Pyramid work in progress 2.0

--- 3DGraphicsCodeBase/src/Assignments/Pyramid/app.cpp
+++ 3DGraphicsCodeBase/src/Assignments/Pyramid/app.cpp
@@ -26,24 +26,36 @@ void SimpleShapeApplication::init()
         exit(-1);
     }
 
+    // std::vector<GLfloat> vertices = {
+    //     -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,//0
+    //     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,//1
+    //     0.5f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,//2
+    //     0.5f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,//3
+    //     0.0f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,//4
+    //     -0.5f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,//5
+    //     -0.5f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,//6
+    // };
+    glEnable(GL_DEPTH_TEST);
+    
     std::vector<GLfloat> vertices = {
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,//0
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,//1
-        0.5f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,//2
-        0.5f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,//3
-        0.0f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,//4
-        -0.5f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,//5
-        -0.5f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,//6
+        -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,//0
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,//1
+        0.0f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,//2
+
+        0.0f, 0.5f, 0.0f,   0.0f, 1.0f, 0.0f,//3
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,//4
+        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,//5
+        
+        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f,//6
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,//7
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,//8
+        
+        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 0.0f,//9
+        -0.3f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f,//10
+        0.3f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f,//11
     };
 
-    //  std::vector<GLfloat> vertices = {
-    //     0.0f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,//0
-    //     0.5f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,//1
-    //     0.0f, -0.5f, 0.5f,   1.0f, 0.0f, 0.0f,//2
-    //     -0.5f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,//3
-    // };
-
-    // glEnable(GL_DEPTH_TEST);
+    
 
     GLuint v_buffer_handle;
     glGenBuffers(1, &v_buffer_handle);
@@ -52,11 +64,8 @@ void SimpleShapeApplication::init()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     std::vector<GLushort> indices = {
-        0, 1, 6, 1, 3, 6, 5, 2, 4
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
     };
-    // std::vector<GLushort> indices = {
-    //     0, 1, 2
-    // };
     
     GLuint i_buffer_handle;
     glGenBuffers(1, &i_buffer_handle);
@@ -72,8 +81,10 @@ void SimpleShapeApplication::init()
     glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
     glBindBuffer(GL_UNIFORM_BUFFER, 0); 
     glBindBufferBase(GL_UNIFORM_BUFFER, 0, color_trans_buffer_handle); 
-    float strength = 0.9;
-    float color[3] = {0.98, 0.003, 1.7};
+    // float strength = 0.9;
+    // float color[3] = {0.98, 0.003, 1.7}; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,
+    float strength = 1.0;
+    float color[3] = {1.0, 1.0, 1.0};
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float), &strength);
     glBufferSubData(GL_UNIFORM_BUFFER, 4 * sizeof(float), 3 * sizeof(float), color);
     
@@ -83,9 +94,11 @@ void SimpleShapeApplication::init()
     glGenBuffers(1, &pvm_buffer_handle);
     glBindBuffer(GL_UNIFORM_BUFFER, pvm_buffer_handle);
     glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), nullptr, GL_STATIC_DRAW);
-    glm::mat4 Projection = glm::perspective(glm::half_pi<float>(), (float)w/h, 0.1f, 100.0f);
-    glm::mat4 View = glm::lookAt(glm::vec3{1.0, .5, 2.0}, glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{0.0, 0.0, 1.0});
+    // glm::mat4 Projection = glm::perspective(glm::half_pi<float>(), (float)w/h, 0.1f, 100.0f);
+    // glm::mat4 View = glm::lookAt(glm::vec3{1.0, .5, 2.0}, glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{0.0, 0.0, 1.0});
     glm::mat4 Model = glm::mat4(1.0f);
+    glm::mat4 Projection = glm::perspective(glm::half_pi<float>(), (float)w/h, 0.1f, 100.0f);
+    glm::mat4 View = glm::lookAt(glm::vec3{0.5f, 0.5f, 0.8f}, glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{0.0, 1.0, 0.0});
     glm::mat4 PVM = Projection * View * Model;
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
@@ -126,6 +139,6 @@ void SimpleShapeApplication::frame()
     // Binding the VAO will setup all the required vertex buffers.
     glBindVertexArray(vao_);
     // glDrawArrays(GL_TRIANGLES, 0, 9);
-    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid*>(0));
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid*>(0));
     glBindVertexArray(0);
 }
